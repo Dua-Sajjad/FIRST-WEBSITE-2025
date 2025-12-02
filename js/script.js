699444d0b858041c7e68ca1ecbc2aaa768fa5c78
Dua Sajjad <dua209665@gmail.com>
2025-12-02 13:35:13 +0500

    customize events and membership page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-  function toggleMenu() {
-      document.getElementById('mobile').classList.toggle('active');
-    }
+function toggleMenu() {
+  document.getElementById('mobile').classList.toggle('active');
+}
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  item.addEventListener("click", () => {
+    const p = item.querySelector("p");
+    p.style.display = (p.style.display === "block") ? "none" : "block";
+  });
+});
+
+ function scrollToContact() {
+            document.getElementById("contact").scrollIntoView({
+                behavior: "smooth"
+            });
+        }
